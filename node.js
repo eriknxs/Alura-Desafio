5c647812158c756ddf0c2ef8b357300c59065cc5
Erik Nunes <eriknunes71@gmail.com>
2025-09-17 11:24:35 -0300

    adicionar console

---
+++ node.js
@@ -0,0 +1,11 @@
+console.log("Bom dia")
+function horario(){
+    if(horario==='tarde'){
+        console.log('Boa tarde!');
+    }
+    else{
+        console.log('Boa noite!')
+    }
+}
+let horario='tarde'
+horario()
