ee85f0b5d440bbb05e542f79aee250968b4325fd
Erik Nunes <eriknunes71@gmail.com>
2025-09-17 12:16:40 -0300

    deixar apenas function horario

--- node.js
+++ node.js
@@ -1,12 +1,1 @@
-console.log("Bom dia")
-function horario(){
-    if(horario==='tarde'){
-        console.log('Boa tarde!');
-    }
-    else{
-        console.log('Boa noite!')
-    }
-}
-let horario='tarde'
-horario()
-console.log
+function horario()
